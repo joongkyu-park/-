c293fc51311f5f407a5d2ba5a4b3357419f272d7
joongkyu-park <jk.park.tech@gmail.com>
2022-03-16 00:30:49 +0900

    22.3.14_복습 1문제

--- Graph/boj16940_BFS 스페셜 져지.swift
+++ Graph/boj16940_BFS 스페셜 져지.swift
@@ -1,59 +1,60 @@
 // 출처 : https://www.acmicpc.net/user/golddol2003
 // parent배열의 활용이 시간초과를 줄이는데에 관건. (원래는 배열의 contains 함수를 이용해서 하나하나 찾으려다보니까 시간초과)
 
+// 22.3.13 업데이트
+
 import Foundation
 
 let n = Int(readLine()!)!
 
-var edges: [[Int]] = Array(repeating: [], count: n)
-var parent = Array(repeating: 0, count: n)
+var edges: [[Int]] = Array(repeating: [], count: n+1)
+var parent = Array(repeating: -1, count: n+1)
 
 for _ in 0..<n-1{
     let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-    edges[input[0]-1].append(input[1]-1)
-    edges[input[1]-1].append(input[0]-1)
+    edges[input[0]].append(input[1])
+    edges[input[1]].append(input[0])
 }
-let order = readLine()!.split(separator: " ").map{Int(String($0))!-1}
+let order = readLine()!.split(separator: " ").map{Int(String($0))!}
 
 
 var q = [Int]()
-var check = Array(repeating: false, count: n)
-q.append(0)
-check[0] = true
+var visited = Array(repeating: false, count: n+1)
 
-var index = 1
-var ans = [Int]()
-
-func bfs() {
-    for i in 0..<n{
-        if q.isEmpty{
-            print(0)
-            return
-        }
-        let x = q.removeFirst()
-        if x != order[i]{
-            print(0)
-            return
-        }
-        var cnt = 0
-        for y in edges[x]{
-            if check[y] == false{
-                parent[y] = x
-                cnt += 1
+if order[0] == 1 {
+    q.append(1)
+    parent[1] = -2 //root node
+    visited[1] = true
+    var orderIdx = 1
+    
+    var answer = 1
+    
+    outer: while !q.isEmpty {
+        let node = q.removeFirst()
+        var childCnt = 0
+        for i in 0..<edges[node].count {
+            let childNode = edges[node][i]
+            if parent[childNode] == -1 {
+                parent[childNode] = node
+                childCnt += 1
             }
         }
-        for j in 0..<cnt{
-            if parent[order[index+j]] != x{
-                print(0)
-                return
+        
+        for i in orderIdx..<orderIdx + childCnt {
+            let childNode = order[i]
+            
+            if visited[childNode] == false {
+                if parent[childNode] != node {
+                    answer = 0
+                    break outer
+                }
+                q.append(childNode)
+                visited[childNode] = true
             }
-            q.append(order[index+j])
-            check[order[index+j]] = true
         }
-        index += cnt
+        orderIdx += childCnt
     }
-    print(1)
-    return
+    
+    print(answer)
 }
-
-bfs()
+else {print(0)}
