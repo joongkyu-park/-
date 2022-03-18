1a46443cd584467b10c46ce522abe3f50ae0c5cf
joongkyu-park <jk.park.tech@gmail.com>
2022-03-19 01:56:21 +0900

    22.03.18_KMP 알고리즘 공부, 문자열 1문제

---
+++ String/boj16916_부분 문자열.swift
@@ -0,0 +1,45 @@
+/*
+ KMP 코드 이해에 많은 도움을 준 사이트
+ https://bowbowbow.tistory.com/6
+ https://soobarkbar.tistory.com/80
+ https://bblackscene21.tistory.com/2
+ */
+import Foundation
+
+let s = Array(readLine()!)
+let p = Array(readLine()!)
+
+var pi = Array(repeating: 0, count: p.count)
+
+getPi()
+
+var answer = 0
+var j = 0
+for i in 0..<s.count {
+    while j>0 && s[i] != p[j] {
+        j = pi[j-1]
+    }
+    if s[i] == p[j] {
+        if j == p.count-1 {
+            answer = 1
+            break
+        } else { j += 1 }
+        
+    }
+}
+print(answer)
+
+
+func getPi(){
+    var j = 0
+    for i in 1..<p.count {
+        
+        while (j > 0) && (p[i] != p[j]) {
+            j = pi[j-1]
+        }
+        if p[i] == p[j] {
+            j += 1
+            pi[i] = j
+        }
+    }
+}
